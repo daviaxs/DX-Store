0dc0dc135a45eecbc1974754ec002204a5239617
Davi Alves <cadwvk@gmail.com>
2023-10-18 16:34:35 -0300

    feat: add header menu using sheet component

--- src/shared/components/header/Header.tsx
+++ src/shared/components/header/Header.tsx
@@ -1,13 +1,58 @@
 import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
-import { MenuIcon, ShoppingCartIcon } from 'lucide-react'
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTrigger,
+} from '@/components/ui/sheet'
+import {
+  Home,
+  ListOrderedIcon,
+  LogInIcon,
+  MenuIcon,
+  PercentIcon,
+  ShoppingCartIcon,
+} from 'lucide-react'
 
 export function Header() {
   return (
     <Card className="flex items-center justify-between p-[1.875rem]">
-      <Button size="icon" variant="outline">
-        <MenuIcon />
-      </Button>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button size="icon" variant="outline">
+            <MenuIcon />
+          </Button>
+        </SheetTrigger>
+
+        <SheetContent side="left">
+          <SheetHeader className="text-left text-lg font-semibold">
+            Menu
+          </SheetHeader>
+
+          <div className="mt-2 flex flex-col gap-2">
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <LogInIcon size={16} />
+              Fazer login
+            </Button>
+
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <Home size={16} />
+              Início
+            </Button>
+
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <PercentIcon size={16} />
+              Ofertas
+            </Button>
+
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <ListOrderedIcon size={16} />
+              Catálogo
+            </Button>
+          </div>
+        </SheetContent>
+      </Sheet>
 
       <h1 className="text-lg font-semibold">
         <span className="purple-gradient font-extrabold">DX</span> Store
