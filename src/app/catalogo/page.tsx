5d8f0c5a75f6df224699eff3160d8a027ec5d099
Davi Alves <cadwvk@gmail.com>
2023-10-20 17:47:41 -0300

    feat: added CatalogPage

---
+++ src/app/catalogo/page.tsx
@@ -0,0 +1,26 @@
+import { Badge } from '@/components/ui/badge'
+import { prismaClient } from '@/lib/prisma'
+import { CatalogIcon } from '@/shared/assets/CatalogIcon'
+import { CategoryItem } from './utils/CategoryItem'
+
+export default async function CatalogPage() {
+  const categories = await prismaClient.category.findMany({})
+
+  return (
+    <div className="flex flex-col gap-8 p-5">
+      <Badge
+        className="w-fit gap-1 border-2 border-primary px-4 py-2 text-base font-bold uppercase"
+        variant="outline"
+      >
+        <CatalogIcon size={16} />
+        Catálogo
+      </Badge>
+
+      <div className="grid grid-cols-2 gap-8">
+        {categories.map((category) => (
+          <CategoryItem category={category} key={category.id} />
+        ))}
+      </div>
+    </div>
+  )
+}
