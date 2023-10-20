e79d73db293f79e3bd60902bb858c20c4765d299
Davi Alves <cadwvk@gmail.com>
2023-10-20 16:57:46 -0300

    updated text size in footer component

--- src/shared/components/Footer/Footer.tsx
+++ src/shared/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <div className="mt-4 bg-accent px-8 py-4 opacity-50">
+    <div className="mt-4 bg-accent px-8 py-4 text-[0.625rem] opacity-50">
       © 2023 Copyright <span className="font-semibold">DX Store</span>
     </div>
   )
