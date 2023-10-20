45a91f94d56960932caaa421a5b5f5a36c2b4716
Davi Alves <cadwvk@gmail.com>
2023-10-20 18:40:11 -0300

    feat: unsure user goes to category page on category item click

--- src/app/(home)/utils/CategoryItem.tsx
+++ src/app/(home)/utils/CategoryItem.tsx
@@ -1,6 +1,7 @@
 import { Badge } from '@/components/ui/badge'
 import { CATEGOTY_ICON } from '@/shared/constants/categoryIcon'
 import { Category } from '@prisma/client'
+import Link from 'next/link'
 
 interface CategoryItemProps {
   category: Category
@@ -8,12 +9,14 @@ interface CategoryItemProps {
 
 export function CategoryItem({ category }: CategoryItemProps) {
   return (
-    <Badge
-      variant="outline"
-      className="flex items-center justify-center gap-2 rounded-lg py-3"
-    >
-      {CATEGOTY_ICON[category.slug as keyof typeof CATEGOTY_ICON]}
-      <span className="text-xs font-bold">{category.name}</span>
-    </Badge>
+    <Link href={`/categoria/${category.slug}`}>
+      <Badge
+        variant="outline"
+        className="flex items-center justify-center gap-2 rounded-lg py-3"
+      >
+        {CATEGOTY_ICON[category.slug as keyof typeof CATEGOTY_ICON]}
+        <span className="text-xs font-bold">{category.name}</span>
+      </Badge>
+    </Link>
   )
 }
