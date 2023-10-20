b0d54367d4c88b7cb54b6a101ab129da5ece31e5
Davi Alves <cadwvk@gmail.com>
2023-10-20 18:20:24 -0300

    feat: add page to show category products

--- src/app/catalogo/utils/CategoryItem.tsx
+++ src/app/catalogo/utils/CategoryItem.tsx
@@ -1,5 +1,6 @@
 import { Category } from '@prisma/client'
 import Image from 'next/image'
+import Link from 'next/link'
 
 interface CategoryItemsProps {
   category: Category
@@ -7,24 +8,26 @@ interface CategoryItemsProps {
 
 export function CategoryItem({ category }: CategoryItemsProps) {
   return (
-    <div className="flex flex-col">
-      <div className="bg-category-item-gradient flex h-[150px] w-full items-center justify-center rounded-t-lg">
-        <Image
-          src={category.imageUrl}
-          alt={category.name}
-          width={0}
-          height={0}
-          sizes="100vw"
-          className="h-auto max-h-[70%] w-auto max-w-[80%]"
-          style={{
-            objectFit: 'contain',
-          }}
-        />
-      </div>
+    <Link href={`/categoria/${category.slug}`}>
+      <div className="flex flex-col">
+        <div className="bg-category-item-gradient flex h-[150px] w-full items-center justify-center rounded-t-lg">
+          <Image
+            src={category.imageUrl}
+            alt={category.name}
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="h-auto max-h-[70%] w-auto max-w-[80%]"
+            style={{
+              objectFit: 'contain',
+            }}
+          />
+        </div>
 
-      <div className="rounded-b-lg bg-accent py-3">
-        <p className="text-semibold text-center text-sm">{category.name}</p>
+        <div className="rounded-b-lg bg-accent py-3">
+          <p className="text-semibold text-center text-sm">{category.name}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   )
 }
---
+++ src/app/categoria/[slug]/page.tsx
@@ -0,0 +1,41 @@
+import { Badge } from '@/components/ui/badge'
+import { prismaClient } from '@/lib/prisma'
+import { ProductItem } from '@/shared/components/productItem/ProductItem'
+import { CATEGOTY_ICON } from '@/shared/constants/categoryIcon'
+import { computeProductTotalPrice } from '@/shared/helpers/product'
+
+export default async function CategoryProduct({ params }: any) {
+  const category = await prismaClient.category.findFirst({
+    where: {
+      slug: params.slug,
+    },
+    include: {
+      products: true,
+    },
+  })
+
+  if (!category) {
+    return null
+  }
+
+  return (
+    <div className="flex flex-col gap-8 p-5">
+      <Badge
+        className="w-fit gap-1 border-2 border-primary px-4 py-2 text-base font-bold uppercase"
+        variant="outline"
+      >
+        {CATEGOTY_ICON[params.slug as keyof typeof CATEGOTY_ICON]}
+        {category.name}
+      </Badge>
+
+      <div className="grid grid-cols-2 gap-8">
+        {category.products.map((product) => (
+          <ProductItem
+            product={computeProductTotalPrice(product)}
+            key={product.id}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
