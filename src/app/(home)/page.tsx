d2f1e6419ac3584c7deba78fc7adeb755a280fb8
Davi Alves <cadwvk@gmail.com>
2023-10-20 09:56:54 -0300

    feat: add second banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -28,8 +28,18 @@ export default async function Home() {
       </div>
 
       <div className="mt-8">
+        <p className="mb-2 px-5 font-bold uppercase">Ofertas</p>
         <ProductList products={deals} />
       </div>
+
+      <Image
+        src="/banner_home_02.png"
+        alt="Até 55% de desconto em mouses!"
+        height={0}
+        width={0}
+        className="mt-2 h-auto w-full px-5"
+        sizes="100vw"
+      />
     </div>
   )
 }
