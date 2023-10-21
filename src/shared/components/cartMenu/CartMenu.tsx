717a266506fc394e1ff653e31d0d6e55d44f6a54
Davi Alves <cadwvk@gmail.com>
2023-10-21 19:37:14 -0300

    feat: added CartItem component and improved cart product addition

--- src/shared/components/cartMenu/CartMenu.tsx
+++ src/shared/components/cartMenu/CartMenu.tsx
@@ -4,8 +4,10 @@ import { Badge } from '@/components/ui/badge'
 import { Button } from '@/components/ui/button'
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
 import { CartContext } from '@/providers/cart'
+import { computeProductTotalPrice } from '@/shared/helpers/product'
 import { ShoppingCartIcon } from 'lucide-react'
 import { useContext } from 'react'
+import { CartItem } from './utils/CartItem'
 
 export function CartMenu() {
   const { products } = useContext(CartContext)
@@ -19,7 +21,7 @@ export function CartMenu() {
       </SheetTrigger>
 
       <SheetContent>
-        <div>
+        <div className="flex flex-col gap-8">
           <Badge
             className="w-fit gap-1 border-2 border-primary px-4 py-2 text-sm font-bold uppercase"
             variant="outline"
@@ -28,9 +30,14 @@ export function CartMenu() {
             Carrinho
           </Badge>
 
-          {products.map((product) => (
-            <h1 key={product.id}>{product.name}</h1>
-          ))}
+          <div className="flex flex-col gap-5">
+            {products.map((product) => (
+              <CartItem
+                key={product.id}
+                product={computeProductTotalPrice(product as any) as any}
+              />
+            ))}
+          </div>
         </div>
       </SheetContent>
     </Sheet>
