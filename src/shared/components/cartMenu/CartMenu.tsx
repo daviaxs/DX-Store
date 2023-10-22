ddac049cfc00777c147f186598cd893039e0fb7e
Davi Alves <cadwvk@gmail.com>
2023-10-22 14:22:11 -0300

    feat: unsure cart products can be scrolled

--- src/shared/components/cartMenu/CartMenu.tsx
+++ src/shared/components/cartMenu/CartMenu.tsx
@@ -2,6 +2,7 @@
 
 import { Badge } from '@/components/ui/badge'
 import { Button } from '@/components/ui/button'
+import { ScrollArea } from '@/components/ui/scroll-area'
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
 import { CartContext } from '@/providers/cart'
 import { computeProductTotalPrice } from '@/shared/helpers/product'
@@ -22,7 +23,7 @@ export function CartMenu() {
       </SheetTrigger>
 
       <SheetContent>
-        <div className="flex flex-col gap-8">
+        <div className="flex h-full flex-col gap-8">
           <Badge
             className="w-fit gap-1 border-2 border-primary px-4 py-2 text-sm font-bold uppercase"
             variant="outline"
@@ -31,24 +32,26 @@ export function CartMenu() {
             Carrinho
           </Badge>
 
-          <div className="flex flex-col gap-5">
-            {products.length > 0 ? (
-              products.map((product) => (
-                <CartItem
-                  key={product.id}
-                  product={computeProductTotalPrice(product as any) as any}
-                />
-              ))
-            ) : (
-              <p className="text-center font-bold opacity-75">
-                Carrinho vazio.
-              </p>
-            )}
+          <div className="flex h-full max-h-full flex-col gap-5 overflow-hidden">
+            <ScrollArea className="h-full">
+              <div className="flex h-full flex-col gap-4">
+                {products.length > 0 ? (
+                  products.map((product) => (
+                    <CartItem
+                      key={product.id}
+                      product={computeProductTotalPrice(product as any) as any}
+                    />
+                  ))
+                ) : (
+                  <p className="text-center font-bold opacity-75">
+                    Carrinho vazio.
+                  </p>
+                )}
+              </div>
+            </ScrollArea>
           </div>
 
-          <Separator />
-
-          <div className="flex flex-col gap-3">
+          <div className="flex flex-col gap-3 bg-zinc-950">
             <div className="flex items-center justify-between text-xs">
               <p>Subtotal</p>
               <p>R$ {subTotal.toFixed(2).replace('.', ',')}</p>
@@ -74,6 +77,10 @@ export function CartMenu() {
               <p>Total</p>
               <p>R$ {total.toFixed(2).replace('.', ',')}</p>
             </div>
+
+            <Button className="mt-5 font-bold uppercase">
+              Finalizar compra
+            </Button>
           </div>
         </div>
       </SheetContent>
