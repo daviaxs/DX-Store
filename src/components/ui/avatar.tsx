cf2a9ae29d243ade2a06e9d21724f975e640b37d
Davi Alves <cadwvk@gmail.com>
2023-10-23 15:08:16 -0300

    fix: disabled prettier in shadcn components

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -1,7 +1,8 @@
+/* eslint-disable prettier/prettier */
 "use client"
 
-import * as React from "react"
 import * as AvatarPrimitive from "@radix-ui/react-avatar"
+import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
@@ -47,4 +48,5 @@ const AvatarFallback = React.forwardRef<
 ))
 AvatarFallback.displayName = AvatarPrimitive.Fallback.displayName
 
-export { Avatar, AvatarImage, AvatarFallback }
+export { Avatar, AvatarFallback, AvatarImage }
+
--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 'use client'
 
 import * as SheetPrimitive from '@radix-ui/react-dialog'
@@ -144,5 +145,5 @@ export {
   SheetOverlay,
   SheetPortal,
   SheetTitle,
-  SheetTrigger,
+  SheetTrigger
 }
