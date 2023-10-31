8d220e0b95e6fbfc748fcea29f470f2b75c35fed
Davi Alves <cadwvk@gmail.com>
2023-10-31 15:43:39 -0300

    feat: added scrollbar in ProductList component

--- src/shared/components/productList/ProductList.tsx
+++ src/shared/components/productList/ProductList.tsx
@@ -1,6 +1,7 @@
 import { ProductItem } from '@/shared/components/productItem/ProductItem'
 import { computeProductTotalPrice } from '@/shared/helpers/product'
 import { Product } from '@prisma/client'
+import './ProductListScrollbar.css'
 
 interface ProductListProps {
   products: Product[]
@@ -8,7 +9,7 @@ interface ProductListProps {
 
 export function ProductList({ products }: ProductListProps) {
   return (
-    <div className="flex w-full gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden">
+    <div className="productListSCrollbar flex w-full gap-4 overflow-x-auto px-5">
       {products.map((product) => (
         <div key={product.id} className="max-w-170px w-[170px]">
           <ProductItem
